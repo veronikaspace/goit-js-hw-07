c95f9162bb46dec01a0c2b0058fe9f986a4ee858
Veronika Space <veronspaceless@gmail.com>
2025-06-22 19:47:40 +0300

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+colorBtn.addEventListener('click', () => {
+  const randomNewColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomNewColor;
+  colorSpan.textContent = randomNewColor;
+});
