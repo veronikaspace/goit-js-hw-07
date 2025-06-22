9dbe08dfca828420f918fe94c14d7fcb4531375e
Veronika Space <veronspaceless@gmail.com>
2025-06-22 19:16:06 +0300

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const { email, password } = event.target.elements;
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (emailValue === '' || passwordValue === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    }
+
+    console.log(formData);
+  event.currentTarget.reset();
+}
